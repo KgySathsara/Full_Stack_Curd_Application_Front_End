a622e76e4f4608a247c74b52c29191ea89f0569b
KgySathsara <88894196+KgySathsara@users.noreply.github.com>
2024-01-14 15:22:36 +0530

    NavBar Change

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="#">Kgy Sathsara</Link>
+                <Link className="navbar-brand" href="#">Full Stack Curd Application</Link>
 
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
